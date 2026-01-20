fa9dc6e00b683991b1967aa70736b55a90ae7734
Deredaya03 <capederedaya@gmail.com>
2026-01-19 23:06:23 -0600

    Creacion de Roadmap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://deredaya.com',
+  output: 'server',
+  adapter: vercel(),
+});
---
+++ src/context/header/components/DataHeader.ts
@@ -0,0 +1,13 @@
+import Home from '../icons/home.astro';
+import Cursos from '../icons/cursos.astro';
+import Dashboard from '../icons/dashboard.astro';
+import Certificados from '../icons/certificados.astro';
+import Areas from '../icons/areas.astro';
+
+export const Seciones = [
+  { href: '/inicio', text: 'Inicio', icon: Home },
+  { href: '/cursos', text: 'Cursos', icon: Cursos },
+  { href: '/dashboard', text: 'Dashboard', icon: Dashboard },
+  { href: '/certificados', text: 'Certificados', icon: Certificados },
+  { href: '/areas', text: 'Areas', icon: Areas },
+];
